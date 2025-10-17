dcfe950792c5e448766a4c8026876d56826b6205
agranjon <agranjon@student.42.fr>
2025-10-17 13:22:37 +0200

    add bulma css, add upload button and title

--- index.php
+++ index.php
@@ -4,9 +4,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>A-Uploader</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
     </head>
     
     <body>
-        
+        <div class="is-flex is-align-items-center is-flex-direction-column mt-5">
+            <h1 class="title">A-Uploader</h1>
+            <p>A simple file uploader</p>
+            <div class="mt-3">
+                <button class="button is-success">Upload file</button>
+            </div>
+        </div>
     </body>
 </html>
